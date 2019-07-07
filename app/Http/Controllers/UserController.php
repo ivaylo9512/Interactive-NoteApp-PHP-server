f1af510f726e916c9711aa0a430589a8cae2f666
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2019-07-07 21:08:27 +0300

    userController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+class UserController extends Controller
+{
+    public function findAll()
+    {
+        $users = User::all();
+        return UserResource::collection($users);
+    }
+    public function findById($id)
+    {
+        $users = User::findOrFail($id);
+        return UserResource::collection($users);
+    }
+    public function update(Request $request, $id){
+        User::whereId($id)->update($request);
+    }
+    public function delete($id){
+        $user = User::findOrFail($id);
+        $user->delete();
+    }
+}
