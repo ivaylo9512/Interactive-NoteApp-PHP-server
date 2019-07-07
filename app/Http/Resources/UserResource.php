8a375d1d0e3fa8d38d1dfc82d068b5534d612a11
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2019-07-07 21:13:46 +0300

    userResource added

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+          'id' => $this -> id,
+          'username' => $this -> username,
+          'firstname' => $this -> firstname,
+          'lastname' => $this -> lastname,
+          'age' => $this -> age,
+          'role' => $this -> role,
+          'profile_picture' => $this -> profile_picture,
+          'country' => $this -> country,
+        ];
+    }
+}
